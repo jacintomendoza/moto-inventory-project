483967a87ed7d42b80f4637d35e55d51d37ed237
unknown <john.meng014@gmail.com>
2022-03-25 16:05:18 -0400

    initial edits

--- src/main/java/com/jms/motoinventoryapi/controller/motoController.java
+++ src/main/java/com/jms/motoinventoryapi/controller/motoController.java
@@ -6,23 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 //@CrossOrigin // may need?
 @RestController
+@RequestMapping("/motorbikes")
 public class motoController {
 
     @Autowired
     private motoRepository motoRepo;
 
-    @GetMapping(value="/")
+    @GetMapping()
     public String getPage() {
         return "Welcome";
     }
 
-    @PostMapping(value = "/motorbike")
+    @PostMapping(path="newbike")
     public void saveMoto(@RequestBody Moto moto){
         motoRepo.save(moto);
     }
