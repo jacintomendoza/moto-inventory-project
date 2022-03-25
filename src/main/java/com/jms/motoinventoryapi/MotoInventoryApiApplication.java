f0b487bcf2198bcc2f4c3e71023273b6172dc5df
Josh Mendoza <jjm121197@gmail.com>
2022-03-25 12:02:13 -0500

    initial commit

---
+++ src/main/java/com/jms/motoinventoryapi/MotoInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.jms.motoinventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoInventoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoInventoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jms/motoinventoryapi/MotoInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jms.motoinventoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
