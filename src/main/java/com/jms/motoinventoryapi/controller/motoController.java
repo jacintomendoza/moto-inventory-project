ff4de4530073411c291c455c7efaf291e92312ac
jacintomendoza <jacinto.josh.mendoza@gmail.com>
2022-03-29 10:02:01 -0500

    cors

--- src/main/java/com/jms/motoinventoryapi/controller/motoController.java
+++ src/main/java/com/jms/motoinventoryapi/controller/motoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@CrossOrigin // may need?
+@CrossOrigin
 @RestController
 @RequestMapping("/motorbikes")
 public class motoController {
