c462cfc8fba47330fcf3b5dd3e0d20a4653d1340
jacintomendoza <jacinto.josh.mendoza@gmail.com>
2022-03-29 18:44:37 -0500

    added table on search

--- moto-frontend/src/app/motos/motos.component.ts
+++ moto-frontend/src/app/motos/motos.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MotoService } from '../moto.service';
-import { Moto } from '../moto.model';
+
 
 @Component({
   selector: 'app-motos',
@@ -8,33 +7,9 @@ import { Moto } from '../moto.model';
   styleUrls: ['./motos.component.scss']
 })
 export class MotosComponent implements OnInit {
-  motos: Moto[] = [];
-  selectedValue = 'Please select';
-  values = ['Make', 'VIN', 'Type'];
-  searchTerm = "";
-  // @Input() 
 
-  constructor(private motoService: MotoService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.motoService.getMotos().subscribe((motos) => {
-      this.motos = motos;
-    })
   }
-
-  test(valuetest: string) {
-    console.log("this is a test!" + valuetest);
-    
-  }
-
-  selectValue(selection: string){
-    this.selectedValue = selection;
-  }
-
-  searchVin(searchTerm: string) {
-    this.motoService.searchVin(searchTerm).subscribe(payload => {
-      this.motos = payload;
-    })
-  }
-
 }
